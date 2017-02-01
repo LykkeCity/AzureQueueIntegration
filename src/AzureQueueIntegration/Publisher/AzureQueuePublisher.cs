91978a789a23be81a0f0e5d4cde5a462efeda141
amigin <amigin@gmail.com>
2017-02-01 02:49:44 +0200

    Add project files.

---
+++ src/AzureQueueIntegration/Publisher/AzureQueuePublisher.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Threading.Tasks;
+using Common;
+using Common.Log;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+
+namespace AzureQueueIntegration.Publisher
+{
+
+    public interface IAzureQueueSerializer<in TModel>
+    {
+        string Serialize(TModel model);
+    }
+
+    public class AzureQueuePublisher<TModel> : IStarter, IMessageProducer<TModel>
+    {
+        private readonly string _applicationName;
+        private readonly AzureQueueSettings _settings;
+
+        private IAzureQueueSerializer<TModel> _serializer;
+        private ILog _log;
+
+        public AzureQueuePublisher(string applicationName, AzureQueueSettings settings)
+        {
+            _applicationName = applicationName;
+            _settings = settings;
+            _settings.QueueName = _settings.QueueName.ToLower();
+        }
+
+        #region Config
+
+        private AzureQueuePublisher<TModel> SetLogger(ILog log)
+        {
+            _log = log;
+            return this;
+        }
+
+        private AzureQueuePublisher<TModel> SetSerializer(IAzureQueueSerializer<TModel> serializer)
+        {
+            _serializer = serializer;
+            return this;
+        }
+
+
+        #endregion
+
+
+        private Task _task;
+
+        private async Task TheTask()
+        {
+
+            while (_task != null)
+                try
+                {
+
+                    var storageAccount = CloudStorageAccount.Parse(_settings.ConnectionString);
+                    var queueClient = storageAccount.CreateCloudQueueClient();
+                    var queue = queueClient.GetQueueReference(_settings.QueueName);
+                    await queue.CreateIfNotExistsAsync();
+
+
+                    while (true)
+                    {
+                        var message = _queue.Dequeue();
+                        if (message == null)
+                        {
+                            if (_task == null)
+                                break;
+
+                            await Task.Delay(1000);
+                            continue;
+                        }
+
+                        var dataToQueue = _serializer.Serialize(message.Item);
+                        await queue.AddMessageAsync(new CloudQueueMessage(dataToQueue));
+                        message.Compliete();
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    await _log.WriteErrorAsync(_applicationName, "TheTask", "", e);
+                }
+        }
+
+
+        public AzureQueuePublisher<TModel> Start()
+        {
+            if (_task != null)
+                return this;
+
+            if (_log == null)
+                throw new Exception("ILog required for: "+_applicationName);
+
+            if (_serializer == null)
+                throw new Exception("IAzureQueueSerializer required for: " + _applicationName);
+
+            _task = TheTask();
+
+            return this;
+        }
+
+        void IStarter.Start()
+        {
+            Start();
+        }
+
+
+        public void Stop()
+        {
+            if (_task == null)
+                return;
+
+            var task = _task;
+            _task = null;
+            task.Wait();
+        }
+
+        private readonly QueueWithConfirmation<TModel> _queue = new QueueWithConfirmation<TModel>();
+
+        public Task ProduceAsync(TModel message)
+        {
+            _queue.Enqueue(message);
+            return Task.FromResult(0);
+        }
+    }
+}
---
+++ src/AzureQueueIntegration/Subscriber/AzureQueueSubscriber.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Common;
+using Common.Log;
+using Microsoft.WindowsAzure.Storage;
+
+namespace AzureQueueIntegration.Subscriber
+{
+
+    public interface IAzureQueueMessageDeserializer<out TModel>
+    {
+        TModel Deserialize(string data);
+    }
+
+    public class AzureQueueSubscriber<TModel> : IStarter, IMessageConsumer<TModel>
+    {
+        private readonly string _applicationName;
+        private readonly AzureQueueSettings _settings;
+        private IAzureQueueMessageDeserializer<TModel> _deserializer;
+        private ILog _log;
+
+        public AzureQueueSubscriber(string applicationName, AzureQueueSettings settings)
+        {
+            _applicationName = applicationName;
+            _settings = settings;
+        }
+
+
+        #region Configure
+
+        public AzureQueueSubscriber<TModel> SetDeserializer(IAzureQueueMessageDeserializer<TModel> deserializer)
+        {
+            _deserializer = deserializer;
+            return this;
+        }
+
+        public AzureQueueSubscriber<TModel> SetLogger(ILog log)
+        {
+            _log = log;
+            return this;
+        }
+
+        #endregion
+
+        private Task _task;
+
+        private async Task TheTask()
+        {
+            while (_task != null)
+                try
+                {
+
+                    var storageAccount = CloudStorageAccount.Parse(_settings.ConnectionString);
+                    var queueClient = storageAccount.CreateCloudQueueClient();
+                    var queue = queueClient.GetQueueReference(_settings.QueueName);
+                    await queue.CreateIfNotExistsAsync();
+
+                    while (_task != null)
+                    {
+
+
+                        var messages = (await queue.GetMessagesAsync(31)).ToArray();
+
+                        if (messages.Length == 0)
+                            await Task.Delay(1000);
+                        else
+                            foreach (var message in messages)
+                            {
+                                var data = _deserializer.Deserialize(message.AsString);
+                                await Task.WhenAll(_callbacks.Select(itm => itm(data)));
+                                await queue.DeleteMessageAsync(message);
+                            }
+
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    await _log.WriteErrorAsync(_applicationName, "TheTask", "", e);
+                }
+
+        }
+
+
+        public AzureQueueSubscriber<TModel> Start()
+        {
+            if (_task != null)
+                return this;
+
+
+            if (_deserializer == null)
+                throw new Exception("Deserializer required for: "+_applicationName);
+
+            if (_log == null)
+                throw new Exception("ILog required for: " + _applicationName);
+
+            _task = TheTask();
+
+            return this;
+        }
+
+        void IStarter.Start()
+        {
+            Start();
+        }
+
+        public void Stop()
+        {
+            if (_task == null)
+                return;
+
+            var task = _task;
+            _task = null;
+            task.Wait();
+        }
+
+        private readonly List<Func<TModel, Task>> _callbacks = new List<Func<TModel, Task>>();
+
+        public void Subscribe(Func<TModel, Task> callback)
+        {
+            _callbacks.Add(callback);
+        }
+
+    }
+}
